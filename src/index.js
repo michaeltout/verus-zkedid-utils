f01d5c06c6577c06a5edfed1e09ad56b8ac80950
Michael Toutonghi <toutonghi.michael@gmail.com>
2020-05-13 12:18:00 +0200

    Add utf8toHash160 to module exports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const StructuredMemo = require('./classes/StructuredMemo')
 const PresetObjects = require('./classes/MemoTypes/index')
 const Types = require('./utils/data_types/index')
+const { utf8ToHash160 } = require('./utils/hash')
 
 
 module.exports = {
   StructuredMemo,
   PresetObjects,
-  Types
+  Types,
+  utf8ToHash160
 }
